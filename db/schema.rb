98c8e74f6a3ae3a477680bb75168b375aebfaf9a
Richard Traindl <richard.traindl@chello.at>
2012-03-03 14:07:20 +0100

    datenmodell und css geaendert

--- db/schema.rb
+++ db/schema.rb
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(:version => 20120210144514) do
 
   create_table "behandlungen", :force => true do |t|
     t.integer  "tier_id",                                                            :null => false
-    t.datetime "behandlungsdatum",                :default => '2012-03-02 20:43:24', :null => false
+    t.datetime "behandlungsdatum",                :default => '2012-03-03 13:00:51', :null => false
     t.string   "gewicht_kg",       :limit => 20
     t.text     "diagnose"
     t.string   "laborwerte1",      :limit => 200
