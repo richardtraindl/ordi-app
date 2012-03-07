2a4fedd6b257c46c8969831411d47750744937b9
richardtraindl <richard.traindl@chello.at>
2012-03-07 11:58:42 +0100

    change theme jqueryui

--- db/schema.rb
+++ db/schema.rb
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(:version => 20120210144514) do
 
   create_table "behandlungen", :force => true do |t|
     t.integer  "tier_id",                                                            :null => false
-    t.datetime "behandlungsdatum",                :default => '2012-03-06 19:48:57', :null => false
+    t.datetime "behandlungsdatum",                :default => '2012-03-07 10:25:26', :null => false
     t.string   "gewicht_kg",       :limit => 20
     t.text     "diagnose"
     t.string   "laborwerte1",      :limit => 200
