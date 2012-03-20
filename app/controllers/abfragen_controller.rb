b148f9afb5be307cfa35f68ca4b3d7dab1ed0e49
richardtraindl <richard.traindl@chello.at>
2012-03-20 17:07:54 +0100

    abfragen umgemodelt

--- app/controllers/abfragen_controller.rb
+++ app/controllers/abfragen_controller.rb
@@ -1,94 +1,105 @@
+# encoding: utf-8
 
 require 'csv'
-
+    
 
 class AbfragenController < ApplicationController
 
+  include ApplicationHelper
+
   def index
-    if params[:abfrage].nil?
-      @karteikarten = []
-      @abfrage = Abfrage.first
-    else
-      @abfrage = Abfrage.find(params[:abfrage].to_i)
+		@karteikarten = []
+		
+		if params[:abfrage].nil?
+		    @abfrage = Abfrage.first
+		else
+		    @abfrage = Abfrage.find(params[:abfrage])
 
-      if @abfrage.id == 4 || @abfrage.id == 9 || @abfrage.id == 10
-        # params[:param1].nil? ? @param1 = "" : @param1 = params[:param1]
-        # params[:param2].nil? ? @param2 = "" : @param2 = params[:param2]
-        @karteikarten = []
-        flash.now[:error] = "Datum fehlt!"
-      else
-        params[:param1].nil? ? @param1 = "" : @param1 = params[:param1]
-        @param2 = ""
+				params[:param1].nil? ? @param1 = "" : @param1 = params[:param1]
 
-    
-        @persopt = params[:persopt]
-      
-        @tieropt = params[:tieropt]
-          
-        puts ( "###############" + @param1 + @param2 + "###########" )
-        
-        if @abfrage.id == 1 || @abfrage.id == 13 # postadresse
-          @karteikarten = Karteikarte.find(:all, :conditions => [@abfrage.bedingung, @param1 + "%"], :joins => [:tier, :person, :person => :postadressen])
-        
-        elsif @abfrage.id == 15 # kontakt
-          @karteikarten = Karteikarte.find(:all, :conditions => [@abfrage.bedingung, @param1 + "%"], :joins => [:tier, :person, :person => :kontakte])
-        
-        elsif @abfrage.id == 5 || @abfrage.id == 7 || @abfrage.id == 12	|| @abfrage.id == 14 ||
-              @abfrage.id == 16 || @abfrage.id == 17 || @abfrage.id == 8 #tier.*, person.*
-          @karteikarten = Karteikarte.find(:all, :conditions => [@abfrage.bedingung, @param1 + "%"], :joins => [:tier, :person])
-        
-        elsif @abfrage.id == 2 || @abfrage.id == 3 || @abfrage.id == 6 #arznei, arzneimittel, diagnose
-          @karteikarten = Karteikarte.find(:all, :select => "DISTINCT karteikarten.*", :conditions => [@abfrage.bedingung, @param1 + "%"], :joins => [:tier, :person, :tier => :behandlungen])
+				params[:param2].nil? ? @param2 = "" : @param2 = params[:param2]
+								
+				@persopt = params[:persopt]
+								
+				@tieropt = params[:tieropt]
+									
+				puts ( "###############" + @param1 + @param2 + "###########" )
+				
+				if @abfrage.id == 1 || @abfrage.id == 13 # postadresse
+						params[:param1].blank? ? flash.now[:error] = "Eingabe fehlt!" : @karteikarten = Karteikarte.find(:all, :conditions => [@abfrage.bedingung, @param1 + "%"], :joins => [:tier, :person, :person => :postadressen])
+				
+				elsif @abfrage.id == 15 # kontakt
+						params[:param1].blank? ? flash.now[:error] = "Eingabe fehlt!" : @karteikarten = Karteikarte.find(:all, :conditions => [@abfrage.bedingung, @param1 + "%"], :joins => [:tier, :person, :person => :kontakte])
+				
+				elsif @abfrage.id == 5 || @abfrage.id == 7 || @abfrage.id == 12	|| @abfrage.id == 14 ||
+							@abfrage.id == 16 || @abfrage.id == 17 || @abfrage.id == 8 #tier.*, person.*
+								params[:param1].blank? ? flash.now[:error] = "Eingabe fehlt!" : @karteikarten = Karteikarte.find(:all, :conditions => [@abfrage.bedingung, @param1 + "%"], :joins => [:tier, :person])
+				
+				elsif @abfrage.id == 2 || @abfrage.id == 3 || @abfrage.id == 6 #arznei, arzneimittel, diagnose
+						params[:param1].blank? ? flash.now[:error] = "Eingabe fehlt!" : @karteikarten = Karteikarte.find(:all, :select => "DISTINCT karteikarten.*", :conditions => [@abfrage.bedingung, @param1 + "%"], :joins => [:tier, :person, :tier => :behandlungen])
+				
+				elsif @abfrage.id == 4  || @abfrage.id == 9 #behandlung, finanzamt -> datum
+						if check_date(params[:param1]) && check_date(params[:param2])
+								@karteikarten = Karteikarte.find(:all, :select => "DISTINCT karteikarten.*", :conditions => [@abfrage.bedingung, DateTime.parse( @param1 ), DateTime.parse( @param2 )], :joins => [:tier, :person, :tier => :behandlungen])
+						else
+								flash[:error] = "Falsches oder unvollständiges Datum"
+								# redirect_to abfragen_path() and return
+						end
+		
+				elsif @abfrage.id == 10 #impfung -> datum
+						if check_date(params[:param1]) && check_date(params[:param2])
+								@karteikarten = Karteikarte.find(:all, :select => "DISTINCT karteikarten.*", :conditions => [@abfrage.bedingung, DateTime.parse( @param1 ), DateTime.parse( @param2) ], :joins => [:tier, :person, :tier => {:behandlungen => :impfungswerte}])
+						else
+								flash[:error] = "Falsches oder unvollständiges Datum!"
+								# redirect_to abfragen_path() and return
+						end
+		
+				elsif @abfrage.id == 11 #impfung
+						params[:param1].blank? ? flash.now[:error] = "Eingabe fehlt!" : @karteikarten = Karteikarte.find(:all, :conditions => [@abfrage.bedingung, @param1 + "%"], :joins => [:tier, :person, :tier => {:behandlungen => :impfungswerte}])
+				
+				end
+		end
+		
 
-        elsif @abfrage.id == 4  || @abfrage.id == 9 #behandlung, finanzamt -> datum
-          @karteikarten = Karteikarte.find(:all, :select => "DISTINCT karteikarten.*", :conditions => [@abfrage.bedingung, DateTime.parse( @param1 ), DateTime.parse( @param2 )], :joins => [:tier, :person, :tier => :behandlungen])
-
-        elsif @abfrage.id == 10 #impfung -> datum
-          @karteikarten = Karteikarte.find(:all, :select => "DISTINCT karteikarten.*", :conditions => [@abfrage.bedingung, DateTime.parse( @param1 ), DateTime.parse( @param2) ], :joins => [:tier, :person, :tier => {:behandlungen => :impfungswerte}])
-
-        elsif @abfrage.id == 11 #impfung
-          @karteikarten = Karteikarte.find(:all, :conditions => [@abfrage.bedingung, @param1 + "%"], :joins => [:tier, :person, :tier => {:behandlungen => :impfungswerte}])
-        end
-      end  
-    
-      def csv_content_type
-        case request.user_agent
-          when /windows/i
-            'application/vnd.ms-excel'
-          else
-            'text/csv'
-        end
-      end
-
-      def csv_for(karteikarten)
-        (output = "").tap do
-          CSV.generate(output, :col_sep => ",", :row_sep => "\n") do |csv|
-            csv << ["Anredewert", "Titel", "Familienname", "Vorname", "Tiername", "Tierart", "Rasse", "Geschl.", "Geb.Datum"]
-            karteikarten.each do |karteikarte|
-              csv << [karteikarte.person.anredewert.wert, 
-                      karteikarte.person.titel, 
-                      karteikarte.person.familienname,
-                      karteikarte.person.vorname,
-                      karteikarte.tier.tiername,
-                      karteikarte.tier.tierart,
-                      karteikarte.tier.rasse,
-                      karteikarte.tier.geschlechtswert.wert,
-                      karteikarte.tier.geburtsdatum ? karteikarte.tier.geburtsdatum : ""]
-            end
-          end
-        end
-      end
-
-      respond_to do |format|
-          format.html
-
-          format.csv do
-            send_data(csv_for(@karteikarten),
-              :type => csv_content_type,
-              :filename => @abfrage.bezeichnung)
-          end
-      end
+    if params[:csv] == 't'
+					send_data(csv_for(@karteikarten),
+						:type => csv_content_type,
+						:filename => @abfrage.bezeichnung) and return
     end
-  end
+    
+    ## render :index
+						
+  end # method index
+  
+private
+
+  	def csv_content_type
+			case request.user_agent
+				when /windows/i
+					'application/vnd.ms-excel'
+				else
+					'text/csv'
+			end
+		end
+
+		def csv_for(karteikarten)
+			(output = "").tap do
+				CSV.generate(output, :col_sep => ",", :row_sep => "\n") do |csv|
+					csv << ["Anredewert", "Titel", "Familienname", "Vorname", "Tiername", "Tierart", "Rasse", "Geschl.", "Geb.Datum"]
+					karteikarten.each do |karteikarte|
+						csv << [karteikarte.person.anredewert.wert, 
+										karteikarte.person.titel, 
+										karteikarte.person.familienname,
+										karteikarte.person.vorname,
+										karteikarte.tier.tiername,
+										karteikarte.tier.tierart,
+										karteikarte.tier.rasse,
+										karteikarte.tier.geschlechtswert.wert,
+										karteikarte.tier.geburtsdatum ? karteikarte.tier.geburtsdatum : ""]
+					end
+				end
+			end
+		end
+
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  
+  def check_date(value)
+    if value.length != 10
+      return false
+    end
+    
+		tag   = value[0..1]
+		monat = value[3..4]
+    jahr  = value[6..9]
+
+    if jahr.to_i < 1900 || monat.to_i < 1 || monat.to_i > 12 || tag.to_i < 1 || tag.to_i > 31
+      return false
+    end
+    
+    return true
+  end 
+
 end
